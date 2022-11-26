707e77cc9d3feade73c45babcf8688de3cedc9f3
Tai Meade <taitaishi123@gmail.com>
2022-11-25 23:21:01 -0500

    Added some more sentences to the typeSpeedTester

--- typeSpeedTesting.py
+++ typeSpeedTesting.py
@@ -38,11 +38,11 @@ def getThingToType():
                                     "I love playing video games. I always have and I always will. I'm not addicted to anything, you are.",
                                     "These sentences are getting dumber by the minute. Thank you for playing my TypeSpeedTester though!",
                                     "Sometimes you need to be tested so good luck typing supercalifragilisticexpialidocious quickly and accurately!",
-                                    "This sentence is going to be extremely long because I feel that your words per minute have been inflated, and along with them, your confidence. You must be reminded that typing is not meant to be fun or easy."
-                                    "Beach-combing replaced wine tasting as his new obsession. Thigh-high in the water, the fisherman’s hope for dinner soon turned to despair."
-                                    "While all her friends were positive that Mary had a sixth sense, she knew she actually had a seventh sense."
-                                    "Giving directions that the mountains are to the west only works when you can see them."
-                                    "If eating three-egg omelets causes weight-gain, budgie eggs are a good substitute."
+                                    "This sentence is going to be extremely long because I feel that your words per minute have been inflated, and along with them, your confidence. You must be reminded that typing is not meant to be fun or easy.",
+                                    "Beach-combing replaced wine tasting as his new obsession. Thigh-high in the water, the fisherman’s hope for dinner soon turned to despair.",
+                                    "While all her friends were positive that Mary had a sixth sense, she knew she actually had a seventh sense.",
+                                    "Giving directions that the mountains are to the west only works when you can see them.",
+                                    "If eating three-egg omelets causes weight-gain, budgie eggs are a good substitute.",
                                     "It was a slippery slope and he was willing to slide all the way to the deepest depths."
                         ]
     }
